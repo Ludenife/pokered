659bba0c82512b4e75ba24e98b888dc92c31736a
Ludenife <39127660+Ludenife@users.noreply.github.com>
2018-07-12 01:16:47 +0200

    Update route10.asm

--- data/wildPokemon/route10.asm
+++ data/wildPokemon/route10.asm
@@ -9,7 +9,7 @@ Route10Mons:
 		db 15,EKANS
 		db 17,VOLTORB
 		db 17,SPEAROW
-		db 13,EKANS
+		db 13,SANDSHREW
 		db 17,EKANS
 	ENDC
 	IF DEF(_BLUE)
@@ -21,7 +21,7 @@ Route10Mons:
 		db 15,SANDSHREW
 		db 17,VOLTORB
 		db 17,SPEAROW
-		db 13,SANDSHREW
+		db 13,EKANS
 		db 17,SANDSHREW
 	ENDC
 	db $00
