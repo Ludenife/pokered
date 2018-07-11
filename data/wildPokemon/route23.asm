574eeb6b1af71afed821a3f48ead1a3b6eabf4d0
Ludenife <39127660+Ludenife@users.noreply.github.com>
2018-07-12 01:24:46 +0200

    Update route23.asm

--- data/wildPokemon/route23.asm
+++ data/wildPokemon/route23.asm
@@ -9,7 +9,12 @@ Route23Mons:
 	db 33,DITTO
 	db 26,SPEAROW
 	db 38,FEAROW
-	db 38,DITTO
+	IF DEF(_RED)
+		db 28,SANDSHREW
+	ENDC
+	IF DEF(_BLUE)
+		db 28,EKANS
+	ENDC
 	db 38,FEAROW
 	IF DEF(_RED)
 		db 41,ARBOK
@@ -18,6 +23,11 @@ Route23Mons:
 		db 41,SANDSLASH
 	ENDC
 	db 43,DITTO
-	db 41,FEAROW
+	IF DEF(_RED)
+		db 41,SANDSLASH
+	ENDC
+	IF DEF(_BLUE)
+		db 41,ARBOK
+	ENDC
 	db 43,FEAROW
 	db $00
