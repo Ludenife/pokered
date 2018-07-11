c7552ca5867f625764a2f308d7f9adc2f380be7a
Ludenife <39127660+Ludenife@users.noreply.github.com>
2018-07-12 01:30:06 +0200

    Update route25.asm

--- data/wildPokemon/route25.asm
+++ data/wildPokemon/route25.asm
@@ -5,7 +5,7 @@ Route25Mons:
 		db 9,KAKUNA
 		db 13,PIDGEY
 		db 12,ODDISH
-		db 13,ODDISH
+		db 13,BELLSPROUT
 		db 12,ABRA
 		db 14,ODDISH
 		db 10,ABRA
@@ -17,7 +17,7 @@ Route25Mons:
 		db 9,METAPOD
 		db 13,PIDGEY
 		db 12,BELLSPROUT
-		db 13,BELLSPROUT
+		db 13,ODDISH
 		db 12,ABRA
 		db 14,BELLSPROUT
 		db 10,ABRA
